b4f6069a7febe54377a652b5dda81b4065aa947a
domkade <ymukade@gmail.com>
2015-09-07 20:12:39 +0900

    [add]キーボード入力による移動に対応

--- src/main.js
+++ src/main.js
@@ -9,6 +9,24 @@ var Player = enchant.Class.create(enchant.Sprite, {
         game.rootScene.addEventListener('touchend', function(e){ player.x = e.x; game.touched = false; });
         game.rootScene.addEventListener('touchmove', function(e){ player.x = e.x; });
         game.rootScene.addChild(this);
+        game.rootScene.addEventListener('enterframe', function() {
+            var speed = 3;
+            if ((game.input.up || game.input.down) && (game.input.right || game.input.left)){
+               speed *= 0.71;
+            }
+            if (game.input.up) {
+                player.y -= speed;
+            }
+            if (game.input.down) {
+                player.y += speed;
+            }
+            if (game.input.right) {
+                player.x += speed;
+            }
+            if (game.input.left) {
+                player.x -= speed;
+            }
+        });
     }
 });
 
