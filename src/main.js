40effcfee2a86fe3eeec472dc7c41a9255dcfe7b
domkade <ymukade@gmail.com>
2015-09-07 22:30:21 +0900

    [add]自機から弾が出るようにした
    Bullet, PlayerBulletクラスを追加、Playerクラスから生成して弾を撃てるようにした

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ var Player = enchant.Class.create(enchant.Sprite, {
         enchant.Sprite.call(this, 16, 16);
         this.image = game.assets['player.png'];
         this.x = x; this.y = y; this.frame = 0;
+	game.keybind(90, 'shot');
         game.rootScene.addEventListener('touchstart', function(e){ player.x = e.x; game.touched = true; });
         game.rootScene.addEventListener('touchend', function(e){ player.x = e.x; game.touched = false; });
         game.rootScene.addEventListener('touchmove', function(e){ player.x = e.x; });
@@ -26,16 +27,45 @@ var Player = enchant.Class.create(enchant.Sprite, {
             if (game.input.left) {
                 player.x -= speed;
             }
+            if(game.input.shot && game.frame % 5 == 0){
+                new PlayerBullet(player.x, player.y);
+            }
         });
+    }
+});
+
+var Bullet = enchant.Class.create(enchant.Sprite, {
+    initialize: function(x, y, speed){
+        enchant.Sprite.call(this, 16, 16);
+        this.x = x; this.y = y; this.frame = 1;
+        this.speed = speed;
+        game.rootScene.addChild(this);
+    },
+    onenterframe:function(){
+        this.y += this.speed;
+        if(this.y > 320 || this.x > 320 || this.x < -this.width || this.y < -this.height){
+            this.remove();
+        }
+    },
+    remove: function(){ game.rootScene.removeChild(this); delete this; }
+});
+
+var PlayerBullet = enchant.Class.create(Bullet, {
+    initialize: function(x, y){
+        Bullet.call(this, x, y, -10);
+        this.image = game.assets['playerBullet.png'];
     }
 });
 
 window.onload = function() {
     game = new Game(320, 320);
-    game.fps = 60; game.score = 0; game.touched = false; game.preload('player.png');
+    game.fps = 60; game.score = 0; game.touched = false;
+    game.preload('player.png');
+    game.preload('playerBullet.png');
     game.onload = function() {
         player = new Player(160, 300);
         game.rootScene.backgroundColor = 'black';
     }
     game.start();
 }
+
